7d877edfac507001a52420b3f7a338d869c520d4
Reyhan Afif Fahri <126700421+Reyhan-Code@users.noreply.github.com>
2024-04-24 15:29:44 +0700

    Push

---
+++ app/src/main/java/com/dicoding/mywidgets/NumberGenerator.kt
@@ -0,0 +1,9 @@
+package com.dicoding.mywidgets
+import java.util.*
+
+internal object NumberGenerator {
+    fun generate(max: Int): Int {
+        val random = Random()
+        return random.nextInt(max)
+    }
+}
